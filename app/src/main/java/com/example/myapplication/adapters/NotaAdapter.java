3b51f22b6d0cb2905be64b48b3155c1929bd1713
Carlos Steeven Jaramillo <carlosjaramillo@percepcion.digital>
2020-10-13 22:17:50 -0500

    Adaptador personalizado V1

--- app/src/main/java/com/example/myapplication/adapters/NotaAdapter.java
+++ app/src/main/java/com/example/myapplication/adapters/NotaAdapter.java
@@ -1,12 +1,15 @@
 package com.example.myapplication.adapters;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.core.content.res.ResourcesCompat;
 
 import com.example.myapplication.R;
 
@@ -44,8 +47,19 @@ public class NotaAdapter extends BaseAdapter {
         }
 
         TextView item_titulo, item_contenido;
+        ImageView item_imagen;
+        String icon = "ic_account_circle_white_18dp";
+
+        item_imagen = view.findViewById(R.id.item_imagen);
         item_titulo = view.findViewById(R.id.item_titulo);
-        //item_contenido = view.findViewById(R.id.item_contenido);
+
+        //Forma dinámica
+        //int id = view.getResources().getIdentifier("com.example.myapplication:drawable/" + icon, null, null);
+        //item_imagen.setImageResource(id);
+
+        //Forma estática
+        Drawable img = ResourcesCompat.getDrawable(view.getResources(), R.drawable.ic_account_circle_white_18dp, null);
+        item_imagen.setImageDrawable(img);
 
         item_titulo.setText(list.get(i));
 
